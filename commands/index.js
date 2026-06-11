27a282f359f1021f4bed430bb638768c3878124b
noo-shelby <tomazgabryellalbuquerquecavalc@gmail.com>
2026-06-11 17:37:06 -0300

    Add files via upload

--- commands/index.js
+++ commands/index.js
@@ -1,1 +1,40 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+const commands = [
+  new SlashCommandBuilder()
+    .setName('criar-painel')
+    .setDescription('Cria um novo painel de tickets interativamente.')
+    .setDefaultMemberPermissions(0x20), // MANAGE_GUILD
+
+  new SlashCommandBuilder()
+    .setName('editar-painel')
+    .setDescription('Edita, envia ou deleta painéis de tickets salvos.')
+    .setDefaultMemberPermissions(0x20),
+
+  new SlashCommandBuilder()
+    .setName('enviar-painel')
+    .setDescription('Envia um painel salvo para um canal.')
+    .setDefaultMemberPermissions(0x20)
+    .addStringOption(opt =>
+      opt.setName('painel')
+        .setDescription('ID do painel a enviar')
+        .setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName('blacklist')
+    .setDescription('Gerencia a blacklist de tickets.')
+    .setDefaultMemberPermissions(0x20)
+    .addSubcommand(sub =>
+      sub.setName('add')
+        .setDescription('Adiciona usuário à blacklist')
+        .addUserOption(opt => opt.setName('usuario').setDescription('Usuário').setRequired(true))
+    )
+    .addSubcommand(sub =>
+      sub.setName('remove')
+        .setDescription('Remove usuário da blacklist')
+        .addUserOption(opt => opt.setName('usuario').setDescription('Usuário').setRequired(true))
+    ),
+];
+
+module.exports = commands.map(c => c.toJSON());
